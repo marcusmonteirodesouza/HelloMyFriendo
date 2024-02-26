6665a5f5dd6d7c66f793f5e0ecb67a2e1da9c9d6
Marcus Vinicius Monteiro de Souza <marcusmonteirodesouza@gmail.com>
2024-02-25 19:52:26 -0500

    Add Auth0

--- HelloMyFriendo/Program.cs
+++ HelloMyFriendo/Program.cs
@@ -1,7 +1,16 @@
+using Auth0.AspNetCore.Authentication;
 using HelloMyFriendo.Client.Pages;
 using HelloMyFriendo.Components;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Auth0. See https://auth0.com/blog/auth0-authentication-blazor-web-apps/
+builder.Services.AddAuth0WebAppAuthentication(options => {
+    options.Domain = builder.Configuration["Auth0:Domain"];
+    options.ClientId = builder.Configuration["Auth0:ClientId"];
+});
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -26,6 +35,21 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.MapGet("/Account/Login", async (HttpContext httpContext, string redirectUri = "/") => {
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+    .WithRedirectUri(redirectUri).Build();
+
+    await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+app.MapGet("/Account/Logout", async (HttpContext httpContext, string redirectUri = "/") => {
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+    .WithRedirectUri(redirectUri).Build();
+
+    await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
